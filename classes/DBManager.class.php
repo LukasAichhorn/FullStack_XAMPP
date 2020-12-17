46ca7696bc9d66b90d7b030ae117d6f9d5d85413
Johannes Gölles <goelles.j@gmail.com>
2020-12-17 15:56:48 +0100

    Modified DBManager Class

---
+++ classes/DBManager.class.php
@@ -0,0 +1,42 @@
+<?php
+class DBManager{
+    private $DB_LOCATION = "localhost";
+    private $DB_USER="root";
+    private $DB_PW= "hl03VAPfORGedk0J";
+    private $DB_NAME = "goellhornDB"; 
+    private $DB;
+
+
+
+function ConnectDB(){
+
+    $this->DB = new mysqli($this->DB_LOCATION,$this->DB_USER,$this->DB_PW,$this->DB_NAME);
+    $DB = $this->DB;
+    if ($DB->connect_errno) {
+        echo "Failed to connect to MySQL: " . $DB->connect_error;
+    }
+    echo $DB->host_info . " Connection success!\n";
+
+}
+
+function insertUser(){
+    $DB = $this->DB;
+
+
+
+}
+
+
+
+function QueryForUser(){
+    
+
+}
+
+function HashPW($PW){
+$PW = hash("sha256",$PW);
+return $PW;
+}
+}
+
+?>
