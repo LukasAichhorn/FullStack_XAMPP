9b999073cf9f9f14b3b04112b8b0552d44303b70
Johannes Gölles <goelles.j@gmail.com>
2020-12-17 18:01:41 +0100

    Insert User added, testfile for DB tests

--- classes/DBManager.class.php
+++ classes/DBManager.class.php
@@ -19,12 +19,47 @@ function ConnectDB(){
 
 }
 
-function insertUser(){
+function validateUser(){
+    //array with POST values as input, validating each, password already hashed by HandleLogin
+}
+
+function insertUser($validData){
+    //validated data in array as input
     $DB = $this->DB;
+    if(!( $stmt = $DB->prepare("INSERT INTO goellhorndb.user(Username,Passwort,Anrede,Vorname,Nachname) VALUES (?,?,?,?,?)"))){
+        echo "Prepare failed: (" . $DB->errno . ") " . $DB->error; 
+    }
+    
+    if (!$stmt->bind_param("sssss", $validData['Username'],$validData['Passwort'],$validData['Anrede'],$validData['Vorname'],$validData['Nachname'])) {
+    echo "Binding Username failed: (" . $stmt->errno . ") " . $stmt->error;
+    }
+    /*  
+    if (!$stmt->bind_param("Passwort", $validData['Passwort'])) {
+        echo "Binding Passwort failed: (" . $stmt->errno . ") " . $stmt->error;
+        }
+    if (!$stmt->bind_param("Anrede", $validData['Anrede'])) {
+        echo "Binding Anrede failed: (" . $stmt->errno . ") " . $stmt->error;
+        }
+    if (!$stmt->bind_param("Vorname", $validData['Vorname'])) {
+        echo "Binding Vorname failed: (" . $stmt->errno . ") " . $stmt->error;
+        }
+    if (!$stmt->bind_param("Nachname", $validData['Nachname'])) {
+        echo "Binding Nachname failed: (" . $stmt->errno . ") " . $stmt->error;
+        }
+        */
+
+    if (!$stmt->execute()) {
+    echo "Execute failed: (" . $stmt->errno . ") " . $stmt->error;
+    }
+
+   
 
 
 
 }
+
+function selectUserPW(){}
+
 
 
 
@@ -40,6 +75,7 @@ function HashPW($PW){
 $PW = hash("sha256",$PW);
 return $PW;
 }
+
 }
 
 ?>
---
+++ test.php
@@ -0,0 +1,18 @@
+<?php
+include 'classes/DBManager.class.php';
+
+$testDBManager = new DBManager;
+
+$testDBManager->ConnectDB();
+
+$testInput = [
+    "Username" => "Heinz",
+    "Passwort" => "pw",
+    "Anrede" => "Herr",
+    "Vorname" => "Hei",
+    "Nachname" => "Nz"
+];
+
+$testDBManager->insertUser($testInput);    
+
+?>
