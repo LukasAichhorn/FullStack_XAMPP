70cbdbad1de41c3e7f013363aa75cead3617d9db
unknown <lukas.aichhorn@gmail.com>
2020-12-20 20:33:21 +0100

    changed login file

--- components/loginForm.comp.php
+++ components/loginForm.comp.php
@@ -1,23 +1,33 @@
-<div class="container-fluid">
-<form action="login.php" method="post">
-
-    <div class="form-group">
-        <label for="Username">Username</label>
-        <input type="text" name="Username" class="form-control" id="Username" required>
-    </div>
-
-    <div class="form-group">
-        <label for="Passwort">Password</label>
-        <input type="password" name="Passwort" class="form-control" id="Passwort" required>
-    </div>   
-
-    
-    <button type="submit" name="Submit" class="btn btn-primary">Submit</button>
-</form>
 <div class="row">
-    <div class="col">
-    <p>Don´t have an account?</p>
-    <a class='link' aria-current='page' href='register.php'>register</a>
+
+<div class="col-2"></div>
+
+<div class="col">
+    <form action="login.php" method="post">
+
+        <div class="form-group m-1">
+            <label for="Username">Username</label>
+            <input type="text" name="Username" class="form-control" id="Username" required>
+        </div>
+
+        <div class="form-group m-1">
+            <label for="Passwort">Password</label>
+            <input type="password" name="Passwort" class="form-control" id="Passwort" required>
+        </div>
+
+        <div class="form-group m-1">
+            <button type="submit" name="Submit" class="btn btn-sm btn-primary">Submit</button>
+        </div>
+
+    </form>
+    <div class="row">
+        <div class="col">
+            <p>Don´t have an account?</p>
+            <a class='link' aria-current='page' href='register.php'>register</a>
+        </div>
     </div>
-</div>
-</div>
+    </div>
+
+    <div class="col-2"></div>
+
+    </div>
--- pages/login.php
+++ pages/login.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 // all includes and requires
 require "../classes/DBManager.class.php";
 require "../classes/Post.class.php";
@@ -11,7 +11,7 @@ require "../classes/validator.class.php";
 
 <?php
 //instantiate UserManager
-$DB = New DBManager();
+$DB = new DBManager();
 $DB->connectDB();
 
 $UserManager = new UserManager();
@@ -22,22 +22,49 @@ $UserManager->handleLogin($DB);
 
 <!doctype html>
 <html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>GoellHorn</title>
-  </head>
-  <body>
-    <!-- +++++++++++++++++++++++++++++++  Navigation +++++++++++++++++++++++++++++++++++ -->
-    <?php include "../components/navigationBar.comp.php"?>
-<div class="container-fluid">
-<?php include "../components/loginForm.comp.php"?>
-</div>
+  <!-- Bootstrap CSS -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+  <link href="https://fonts.googleapis.com/css2?family=Bree+Serif&family=Open+Sans&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="../style.css">
+  <title>GoellHorn</title>
+</head>
+
+<body>
+
+  <!-- +++++++++++++++++++++++++++++++  Navigation +++++++++++++++++++++++++++++++++++ -->
+  <?php include "../components/navigationBar.comp.php" ?>
+
+  <div class="container-fluid">
+    <div class="row login-BG"></div>
+    <div class="row">
+      <div class="col">
+
+      </div>
+
+      <div class="col-6 p-4 minusTop">     
+            <h5>Login:</h5>
+      
+      
+      <?php include "../components/loginForm.comp.php" ?>    
+            
+         
+        </div>
+        
+
+      
+
+      <div class="col">
+
+      </div>
+    </div>
+  </div>
 
 
 
@@ -48,15 +75,19 @@ $UserManager->handleLogin($DB);
 
 
 
-    <!-- Optional JavaScript; choose one of the two! -->
 
-    <!-- Option 1: Bootstrap Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
 
-    <!-- Option 2: Separate Popper and Bootstrap JS -->
-    <!--
+
+  <!-- Optional JavaScript; choose one of the two! -->
+
+  <!-- Option 1: Bootstrap Bundle with Popper -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+
+  <!-- Option 2: Separate Popper and Bootstrap JS -->
+  <!--
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js" integrity="sha384-q2kxQ16AaE6UbzuKqyBE9/u/KzioAlnx2maXQHiDX9d4/zp8Ok3f+M7DPm+Ib6IU" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-pQQkAEnwaBkjpqZ8RU1fF1AKtTcHJwFl3pblpTlHXybJjHpMYo79HY3hIi4NKxyj" crossorigin="anonymous"></script>
     -->
-  </body>
+</body>
+
 </html>
